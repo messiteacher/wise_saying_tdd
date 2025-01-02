c690fb757c5ac0f48adec2676eb1b6f90b694d36
ShinYounHo <suh2963@gmail.com>
2025-01-02 15:50:52 +0900

    work5

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FirstTest {
@@ -20,5 +24,22 @@ public class FirstTest {
 
         // aaa가 출력되는가?
 //        assertThat(result).isEqualTo("aaa");
+    }
+
+    @Test
+    void t3() {
+
+        // 테스트봇 선입력
+        Scanner sc = new Scanner("종료\n");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        TestApp app = new TestApp();
+        app.run();
+
+        assertThat(out.toString()).contains("명언앱을 종료합니다.");
+
+        // 출력값을 체크
     }
 }
