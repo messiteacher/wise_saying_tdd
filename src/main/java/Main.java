979225b0f2baa7b79a4d0b4765998cd6932e9dc7
ShinYounHo <suh2963@gmail.com>
2025-01-02 15:43:12 +0900

    work4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,30 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test2() {
+
+        // 2. 출력문의 값을 문자열로 받아서 봇에게 줄 수 있다.
+        PrintStream origin = System.out;
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        System.out.println("hello");
+
+        System.setOut(origin);
+
+        String str = out.toString();
+        System.out.println(str);
+    }
+
+    public static void test1() {
 
         // 1. 입력을 봇에게 맡길 수 있다.
         Scanner sc = new Scanner("등록\n현재를 사랑하라.\n작자미상");
