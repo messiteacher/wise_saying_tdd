b341728d3793f130a172222b4faa271a3b530939
ShinYounHo <suh2963@gmail.com>
2025-01-03 12:49:13 +0900

    t5 blue

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -36,7 +36,7 @@ public class FirstTest {
 //        app.run();
 
         TestBot testBot = new TestBot();
-        String out = testBot.run("종료");
+        String out = testBot.run("");
 
         assertThat(out).contains("명언앱을 종료합니다.");
 
@@ -47,7 +47,7 @@ public class FirstTest {
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
     void t4() {
 
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
 
         assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
@@ -57,7 +57,11 @@ public class FirstTest {
     @DisplayName("등록 - 명언 1개 입력")
     void t5() {
 
-        String out = TestBot.run("등록\n현재를 사랑하라.\n작자미상\n종료");
+        String out = TestBot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
 
         assertThat(out)
                 .containsSubsequence("명언 : ", "작가 : ");
