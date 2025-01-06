5a729327c8a07fe5f24c5d9dc0c236c87dc6dfa2
ShinYounHo <suh2963@gmail.com>
2025-01-06 14:14:33 +0900

    file t2 g

--- src/test/java/app/standard/FileTest.java
+++ src/test/java/app/standard/FileTest.java
@@ -22,7 +22,7 @@ public class FileTest {
 
         String file = "test.txt";
 
-        Util.File.createFile();
+        Util.File.createFile(file);
 
         assertThat(Files.exists(Paths.get(file)))
                 .isTrue();
