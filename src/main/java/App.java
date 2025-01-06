9576eaffa98e92979dc91bca3b60c0fe4d9c3dd2
ShinYounHo <suh2963@gmail.com>
2025-01-06 09:11:24 +0900

    t10 r

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,18 +19,15 @@ public class App {
         System.out.println("== 명언 앱 ==");
 
         while (true) {
-
-            System.out.print("명령 ) ");
+            System.out.println("명령 ) ");
             String cmd = sc.nextLine();
-
-            if (cmd.equals("종료")) {
-                systemController.exit();
-                break;
-            } else if (cmd.equals("등록")) {
-                wiseSayingController.actionWrite();
-            } else if (cmd.equals("목록")) {
-                wiseSayingController.actionPrint();
+            switch (cmd) {
+                case "종료" -> systemController.exit();
+                case "등록" -> wiseSayingController.actionWrite();
+                case "목록" -> wiseSayingController.actionPrint();
+                default -> System.out.println("올바른 명령이 아닙니다.");
             }
+            if(cmd.equals("종료")) break;
         }
     }
 }
--- src/main/java/domain/wiseSaying/WiseSayingController.java
+++ src/main/java/domain/wiseSaying/WiseSayingController.java
@@ -1,6 +1,5 @@
 package domain.wiseSaying;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
