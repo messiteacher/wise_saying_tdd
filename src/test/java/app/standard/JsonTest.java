a267496091d3c5c47df63ebe297f9df3863078ed
ShinYounHo <suh2963@gmail.com>
2025-01-07 10:38:09 +0900

    json t2 r

---
+++ src/test/java/app/standard/JsonTest.java
@@ -0,0 +1,46 @@
+package app.standard;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JsonTest {
+
+    @Test
+    @DisplayName("Map을 Json으로 변환")
+    void t1() {
+
+
+        Map<String, Object> map = Map.of("name", "홍길동");
+
+        String jsonStr = Util.Json.mapToJson(map);
+
+        assertThat(jsonStr)
+                .isEqualTo("""
+                        {
+                            "name" : "홍길동"
+                        }
+                        """.stripIndent().trim());
+    }
+
+    @Test
+    @DisplayName("Map을 Json으로 변환2 - 속성이 2개")
+    void t2() {
+
+
+        Map<String, Object> map = Map.of("name", "홍길동", "home", "서울");
+
+        String jsonStr = Util.Json.mapToJson(map);
+
+        assertThat(jsonStr)
+                .isEqualTo("""
+                        {
+                            "name" : "홍길동"
+                            "home" : "서울"
+                        }
+                        """.stripIndent().trim());
+    }
+}
