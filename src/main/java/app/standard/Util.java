c119f9832cc85836b6b01e996aec3cbfd855c1e3
ShinYounHo <suh2963@gmail.com>
2025-01-07 11:24:49 +0900

    json t3 b

--- src/main/java/app/standard/Util.java
+++ src/main/java/app/standard/Util.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Util {
 
@@ -112,25 +113,32 @@ public class Util {
 
             jsonBuilder.append("{\n");
 
-            int i = 0;
-            for (String key : map.keySet()) {
+            String str = map.keySet().stream()
+                    .map(k -> map.get(k) instanceof String
+                            ? "    \"%s\" : \"%s\"".formatted(k, map.get(k))
+                            : "    \"%s\" : %s".formatted(k, map.get(k))
+                    ).collect(Collectors.joining(",\n"));
+            jsonBuilder.append(str);
 
-                Object obj = map.get(key);
-
-                if (obj instanceof String) {
-                    String value = (String)map.get(key);
-                    String tmp = "    \"%s\" : " + "\"%s\"";
-                    jsonBuilder.append(tmp.formatted(key, value));
-                } else if (obj instanceof Integer) {
-                    int value = (int)map.get(key);
-                    String tmp = "    \"%s\" : " + "%d";
-                    jsonBuilder.append(tmp.formatted(key, value));
-                }
-
-                if (i == map.size() - 1) break;
-                jsonBuilder.append(",\n");
-                i++;
-            }
+//            int i = 0;
+//            for (String key : map.keySet()) {
+//
+//                Object obj = map.get(key);
+//
+//                if (obj instanceof String) {
+//                    String value = (String)map.get(key);
+//                    String tmp = "    \"%s\" : " + "\"%s\"";
+//                    jsonBuilder.append(tmp.formatted(key, value));
+//                } else if (obj instanceof Integer) {
+//                    int value = (int)map.get(key);
+//                    String tmp = "    \"%s\" : " + "%s";
+//                    jsonBuilder.append(tmp.formatted(key, value));
+//                }
+//
+//                if (i == map.size() - 1) break;
+//                jsonBuilder.append(",\n");
+//                i++;
+//            }
 
             jsonBuilder.append("\n}");
 
