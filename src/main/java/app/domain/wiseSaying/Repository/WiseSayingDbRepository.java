9b27a86eff57fe34daa614c0674a7593dd4571b5
ShinYounHo <suh2963@gmail.com>
2025-01-15 12:29:15 +0900

    dbrepo t3 g

--- src/main/java/app/domain/wiseSaying/Repository/WiseSayingDbRepository.java
+++ src/main/java/app/domain/wiseSaying/Repository/WiseSayingDbRepository.java
@@ -4,6 +4,7 @@ import app.domain.wiseSaying.WiseSaying;
 import app.standard.simpleDb.SimpleDb;
 import app.standard.simpleDb.Sql;
 
+import java.util.List;
 import java.util.Optional;
 
 public class WiseSayingDbRepository {
@@ -70,5 +71,12 @@ public class WiseSayingDbRepository {
                 .delete();
 
         return rst > 0;
+    }
+
+    public List<WiseSaying> findAll() {
+
+        return simpleDb.genSql().append("SELECT *")
+                .append("FROM wise_saying")
+                .selectRows(WiseSaying.class);
     }
 }
