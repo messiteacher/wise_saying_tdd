267343c115dfe03d5b58e86ffc58e2c50258801c
ShinYounHo <suh2963@gmail.com>
2025-01-15 11:35:42 +0900

    test preprocess

--- src/main/java/app/domain/wiseSaying/Repository/WiseSayingDbRepository.java
+++ src/main/java/app/domain/wiseSaying/Repository/WiseSayingDbRepository.java
@@ -10,4 +10,23 @@ public class WiseSayingDbRepository {
     public WiseSayingDbRepository() {
         this.simpleDb = new SimpleDb("localhost", "root", "qkfmtpffhsk10", "wiseSaying__test");
     }
+
+    public void createWiseSayingTable() {
+
+        simpleDb.run("DROP TABLE IF EXISTS wise_saying");
+
+        simpleDb.run("""
+                CREATE TABLE wise_saying (
+                    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                    content VARCHAR(100) NOT NULL,
+                    author VARCHAR(100) NOT NULL
+                )
+                """);
+    }
+
+    public void truncateWiseSayingTable() {
+        simpleDb.run(
+                "TRUNCATE wise_saying"
+        );
+    }
 }
--- src/test/java/app/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
+++ src/test/java/app/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
@@ -3,11 +3,23 @@ package app.domain.wiseSaying.repository;
 import app.domain.wiseSaying.Repository.WiseSayingDbRepository;
 import app.domain.wiseSaying.Repository.WiseSayingFileRepository;
 import app.domain.wiseSaying.Repository.WiseSayingRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class WiseSayingDbRepositoryTest {
 
-    private final WiseSayingDbRepository wiseSayingRepository = new WiseSayingDbRepository();
+    private static WiseSayingDbRepository wiseSayingRepository = new WiseSayingDbRepository();
+
+    @BeforeAll
+    public static void beforeAll() {
+        wiseSayingRepository.createWiseSayingTable();
+    }
+
+    @BeforeEach
+    void truncateWiseSayingTable() {
+        wiseSayingRepository.truncateWiseSayingTable();
+    }
 
     @Test
     public void test() {
