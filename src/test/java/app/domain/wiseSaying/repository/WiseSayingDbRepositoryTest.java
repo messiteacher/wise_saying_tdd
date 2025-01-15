245349dc3b4597a52f58e40e1da3a12883f48471
ShinYounHo <suh2963@gmail.com>
2025-01-15 12:40:24 +0900

    dbrepo t5 r

--- src/test/java/app/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
+++ src/test/java/app/domain/wiseSaying/repository/WiseSayingDbRepositoryTest.java
@@ -111,4 +111,20 @@ public class WiseSayingDbRepositoryTest {
                         ]
                         """.stripIndent().trim());
     }
+
+    @Test
+    @DisplayName("현재 저장된 명언의 개수를 가져오는 count")
+    void t5() {
+
+        WiseSaying wiseSaying1 = new WiseSaying("aaa", "bbb");
+        wiseSayingRepository.save(wiseSaying1);
+
+        WiseSaying wiseSaying2 = new WiseSaying("ccc", "ddd");
+        wiseSayingRepository.save(wiseSaying2);
+
+        int count = wiseSayingRepository.count();
+
+        assertThat(count)
+                .isEqualTo(2);
+    }
 }
